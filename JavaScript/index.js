607a124c7937fd6ed39575a77736ea59188a2741
Tameka Walls <tamekalwalls@gmail.com>
2024-11-12 19:16:36 -0800

    initial commit

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+alert("I will create a folder entitled JavaScript.");
+alert("I will create a file called indexed.html.");
+alert("I will create a file called index.js.");
+alert("Create peudocode of how to solve this exercise");
+alert("I am building a vault that requires three mathematical calculations to generate three codes in a combination.");
+alert("Within the HTML, create three variables.");
+alert("The combination of the lock is 10-40-39.");
+
+console.log(`You have received tShis message because you have been chosen to open an important vault.  Here is the secret combination:`);
+
+// welcome the user to the page
+alert('Welcome to our webpage!!!');
+
+// tell the user that they are awesome
+alert('You are awesome!');
+
+// VARIABLES
+const age = 48;
+console.log(age);
+
+// variable vlues have data types
+const frieseaten = 50 // number
+const message = "My belly is full!";
+const greeting = "Hello! Are you hungry?"
